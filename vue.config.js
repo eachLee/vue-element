e8c69f8f2d69b96903c857b830c3b6b1906f069a
eachLee <eachli@163.com>
2021-11-29 14:42:51 +0800

    配置publickPath

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
   //     : process.env.port === 'test'
   //     ? '/test'
   //     : '/dev',
-  // publicPath: '/',
+  publicPath: '/vue-element',
   outputDir: 'dist', //打包地址 默认dist
   assetsDir: 'static', //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
   filenameHashing: true,
